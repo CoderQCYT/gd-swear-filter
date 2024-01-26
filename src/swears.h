9a93cbe25e97764451c52433dd6ea81169ed865f
CoderQCFromScraatch <qcollier007@icloud.com>
2024-01-25 21:35:06 -0800

    more swears

--- src/swears.h
+++ src/swears.h
@@ -2,7 +2,7 @@
 #include <array>
 using namespace std;
 
-std::array<std::string,3> profanity = {"fuck","shit","bitch"};
+std::array<std::string,19> profanity = {"fuck","shit","bitch", "shitty", "shittiest", "shitti", "fucking", "fucker", "shitter", "bitcher", "bitching", "bitchless", "bitches", "bastard", "twat", "munter", "feck", "arsehole", "asshole"};
 std::array<std::string,23> sexual = {"sperm","sex","sexy","semen","jizz","ejaculate","jacking", "jerking", "edging", "masturbat", "jacked", "jerked", "edged", "penis", "pussy", "cock", "dick", "vajina", "vagina", "cunt", "fingerbang", "bang me", "bang you"};
 std::array<std::string,6> racism = {"nigger","nigga", "niggers","jipped","gypped","peanut gallery"};
 std::array<std::string,33> antilgbtq = {"faggot", "tranny", "hermaphrodite","shemale","heshe", "transgenderist", "transgenderism", "transsexualism", "batty boy", "battyboy", "cocksucker", "gayrope", "homintern", "lesbo", "sissy", "gay rope", "fence-sitter", "fencesitter", "fence sitter", "bulldyke", "bull dyke", "bull-dyke", "chickenhawk", "chicken hawk", "chicken-hawk", "chickenqueen", "chicken queen", "chicken-queen", "chickenplucker", "chicken plucker", "chicken-plucker", "cuntboy", "fag hag"};
