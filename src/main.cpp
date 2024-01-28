504e28ba6745dd221fbe2c6d8e52794b4753f353
CoderQCFromScraatch <qcollier007@icloud.com>
2024-01-27 16:00:33 -0800

    i think i fixed some of it

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/InfoLayer.hpp>
-#include <Geode/modify/LevelCell.hpp>
-#include <Geode/modify/CommentCell.hpp>
 
 #include <regex>
+
+#include <Geode/modify/InfoLayer.hpp>
+
+#include <Geode/modify/LevelCell.hpp>
+
+#include <Geode/modify/CommentCell.hpp>
 
 #include "swears.h"
 
 using namespace geode::prelude;
+using namespace std;
 
 // Taken from StackOverflow, original answer by Manuel Martinez
-static std::string base64_decode(const std::string& in) {
+static std::string base64_decode(const std::string & in) {
   std::string out;
-  std::vector<int> T(256, -1);
+  std::vector < int > T(256, -1);
   for (int i = 0; i < 64; i++) T["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" [i]] = i;
 
   int val = 0, valb = -8;
@@ -31,23 +35,34 @@ static std::string base64_decode(const std::string& in) {
 std::string doFilter(std::string message) {
   std::string result = message;
   std::vector<std::string> swears;
-  if (Mod::get() -> getSettingValue<bool>("filter-profanity")) 
+  if (Mod::get() -> getSettingValue < bool > ("filter-profanity")) 
     for (const auto & swear: profanity) swears.push_back(swear);
-  if (Mod::get() -> getSettingValue<bool>("filter-sexual")) 
+    
+  if (Mod::get() -> getSettingValue < bool > ("filter-sexual")) 
     for (const auto & swear: sexual) swears.push_back(swear);
-  if (Mod::get() -> getSettingValue<bool>("filter-racism")) 
+    
+  if (Mod::get() -> getSettingValue < bool > ("filter-racism")) 
     for (const auto & swear: racism) swears.push_back(swear);
-  if (Mod::get() -> getSettingValue< bool>("filter-antilgbtq"))
-    for (const auto & swear: antilgbtq) swears.push_back(swear);
-  
-  for (const auto & currentWord: swears) {
-    std::string replacement = Mod::get() -> getSettingValue < bool > ("relax-censor") 
-    ? replacement = currentWord[0] + 
-        std::string(currentWord.length() - 2, '*') + 
-        currentWord[currentWord.length() - 1]
-    : std::string(currentWord.length(), '*');
 
-    std::string extraPattern = Mod::get() -> getSettingValue<bool>("strict-mode") ? "\\b" : "";
+  if (Mod::get() -> getSettingValue < bool > ("filter-antilgbtq")) 
+    for (const auto & swear: antilgbtq) swears.push_back(swear);
+    
+  
+  for (const auto& currentWord: swears) {
+    // This is an old failsafe method that isn't needed anymore, but still could be useful.
+    // if (currentWord.empty()) continue;
+  
+    std::string replacement = Mod::get() -> getSettingValue<bool>("relax-censor") 
+    ? std::string(currentWord.length(), '*') 
+    : replacement = currentWord[0] + 
+      std::string(currentWord.length() - 2, '*') + 
+      currentWord[currentWord.length() - 1];
+    
+
+    std::string extraPattern = Mod::get()->getSettingValue<bool>("strict-mode") 
+    ? "\\b" 
+    : "";
+
     std::regex pattern(extraPattern + currentWord + extraPattern, std::regex_constants::icase);
     result = std::regex_replace(result, pattern, replacement);
   }
@@ -55,21 +70,22 @@ std::string doFilter(std::string message) {
 }
 
 class $modify(CommentCell) {
-  void loadFromComment(GJComment* p0) {
+  void loadFromComment(GJComment * p0) {
     if (Mod::get() -> getSettingValue<bool>("censor-comments")) 
-p0 -> m_commentString = doFilter(p0 -> m_commentString);
+      p0 -> m_commentString = doFilter(p0 -> m_commentString);
+    
     CommentCell::loadFromComment(p0);
   }
 };
 
 class $modify(InfoLayer) {
-  bool init(GJGameLevel* p0, GJUserScore* p1, GJLevelList* p2) {
+  bool init(GJGameLevel * p0, GJUserScore * p1, GJLevelList * p2) {
     if (p0 != NULL) {
-      if (Mod::get() -> getSettingValue < bool > ("censor-level-names")) 
+      if (Mod::get() -> getSettingValue<bool>("censor-level-names")) 
         p0 -> m_levelName = doFilter(p0 -> m_levelName.c_str());
       
-      if (Mod::get() -> getSettingValue < bool > ("censor-level-descriptions")) 
-        p0 -> m_levelDesc = doFilter(base64_decode(p0 -> m_levelDesc));
+      if (Mod::get() -> getSettingValue<bool>("censor-level-descriptions")) 
+        p0 -> m_levelDesc = doFilter(p0 -> m_levelDesc.c_str());
       
     }
     return InfoLayer::init(p0, p1, p2);
@@ -81,8 +97,9 @@ class $modify(LevelCell) {
     if (p0 != NULL) {
       if (Mod::get() -> getSettingValue<bool>("censor-level-names")) 
         p0 -> m_levelName = doFilter(p0 -> m_levelName.c_str());
+      
       if (Mod::get() -> getSettingValue<bool>("censor-level-descriptions")) 
-        p0 -> m_levelDesc = doFilter(base64_decode(p0 -> m_levelDesc));
+        p0 -> m_levelDesc = doFilter(p0 -> m_levelDesc.c_str());
       
     }
     return LevelCell::loadFromLevel(p0);
